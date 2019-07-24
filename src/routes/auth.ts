136bb1a7dc5ed3677080732a13e7eac76e0e299c
Dmitry <dmitry.vivdiuk@acmecryptocorp.io>
2019-07-24 23:19:55 +0300

    refactoring

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,27 +1,33 @@
 import express = require("express");
-import * as request from "request";
+import * as request from "request-promise";
 
 let _SERVER_URL: string;
 const router = express.Router();
-const headers = { "content-type": "application/json" };
 
-router.post("/", function(req, res) {
+router.post("/", async function(req, res) {
   const { adminId } = req.body;
 
-  request.post(
-    {
-      headers,
-      url: `${_SERVER_URL}/auth/check`,
-      body: JSON.stringify({ adminId })
-    },
-    (err, httpResponse, body) => {
-      if (err) {
-        console.log("ERROR", err);
-      }
-
-      res.send(body);
+  if (adminId) {
+    try {
+      const optinons = {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        uri: `${_SERVER_URL}/auth/check`,
+        body: {
+          adminId
+        },
+        json: true
+      };
+      const result = await request(optinons);
+      console.log(`* Check user ${adminId} status: ${result.status}`);
+      return res.send(result);
+    } catch (err) {
+      console.log(`* Check user ${adminId} status: ${err}`);
+      return res.send({ status: "ERROR", message: err.message });
     }
-  );
+  } 
+  console.log(`* Check user ${adminId} status: Invalid admin id`);
+  return res.send({ status: "ERROR", message: "Invalid admin id" });
 });
 
 module.exports = (SERVER_URL: string) => {
